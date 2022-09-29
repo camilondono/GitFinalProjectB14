dcc3a2bb207196cec12e008969698673e40b15a7
camilondono <camilondono1216@gmail.com>
2022-09-28 20:48:42 -0400

    2nd commit

--- src/finalProject/finalClass.java
+++ src/finalProject/finalClass.java
@@ -3,7 +3,7 @@ package finalProject;
 public class finalClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is th ecode for the Mercedes Benz");
 
 	}
 
